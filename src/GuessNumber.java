12f86b276e91a8bfbafdeea8562fd7c853ed4385
miservilla <miservilla@gmail.com>
2016-09-03 08:16:41 -0600

    Added 'while loop' to continue playing.

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -12,19 +12,26 @@ public class GuessNumber {
      * @param args Command-Line arguments are ignored.
      */
     public static void main(String [] args){
-        int rand = (int) (Math.random()*10)+1;
+        String stay = "Y";
         Scanner scanIn = new Scanner(System.in);
 
         System.out.print("Please enter your name: ");
         String userName = scanIn.next();
-        System.out.print("Hi there " + userName +", please pick a random number between 1 and 10: ");
-        int number = TryParseIn();
-        System.out.println("You picked " + number + "!");
-        System.out.println("My number was " + rand + "!");
-        if (rand == number){
-            System.out.println("Congratulations, you picked the correct number!");
+
+        while (stay.equalsIgnoreCase("Y")) {
+            System.out.print("Hi there " + userName + ", please pick a number between 1 and 10 (inclusive): ");
+            int rand = (int) (Math.random() * 10) + 1;
+            int number = TryParseIn();
+            System.out.println("You picked " + number + "!");
+            System.out.println("My number was " + rand + "!");
+            if (rand == number) {
+                System.out.println("Congratulations, you picked the correct number!");
+            } else {
+                System.out.println("Sorry, you were wrong.");
+            }
+            System.out.print("Type 'Y' to play again, type any other letter to quit: ");
+            stay = scanIn.next();
         }
-        else System.out.println("Sorry, you were wrong.");
     }
 
 //Method for error control.
@@ -33,12 +40,12 @@ public class GuessNumber {
         int number;
         do {
             while (!sc.hasNextInt()){
-                System.out.printf("Can not parse, please pick a random number between 1 and 10: ");
+                System.out.printf("Not a number between 1 and 10 (inclusive), try again: ");
                 sc.next();
             }
             number = sc.nextInt();
             if ((number < 1) || (number > 10)){
-                System.out.printf("Can not parse, please pick a random number between 1 and 10: ");
+                System.out.printf("Not a number between 1 and 10 (inclusive), try again: ");
             }
         } while (number < 1 || number > 10);
 
